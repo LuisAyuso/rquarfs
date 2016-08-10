5e1248038cb7eefb87753df8fc49ce3f4ac289b0
Luis Ayuso <luis.f.ayuso@gmail.com>
2016-08-10 19:45:59 +0200

    first function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 extern crate glium;
 
 use std::io;
-use std::io::prelude::*;
+use std::io::Read;
+//use std::io::prelude::*;
+use std::fs::{self, File};
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -18,6 +20,19 @@ implement_vertex!(Vertex, position);
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+fn load_shader( path : &str) -> Result<String, io::Error> {
+
+		let mut vs_path = fs::canonicalize(".").unwrap();
+		vs_path.push("shaders");
+		vs_path.push(format! ("{}{}", path, ".glsl"));
+        print!("open: {:?}\n", vs_path);
+
+		let mut f = try!(File::open(vs_path));
+
+		let mut shader_buff = String::new();
+		f.read_to_string(&mut shader_buff);
+        return Ok(shader_buff);
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -25,7 +40,6 @@ implement_vertex!(Vertex, position);
 
 fn main() {
 
-	use std::fs::{self, File};
 
 	let path = fs::canonicalize(".").unwrap();
 	print! ("hello, we are in: {:?}\n", path);
@@ -48,55 +62,41 @@ fn main() {
 	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-//	let (vertex_shader_src, fragment_shader_src) = {
+    let vertex_shader = load_shader("geom.vs");
+    let fragment_shader = load_shader("geom.fs");
 
-		let mut vs_path = fs::canonicalize(".").unwrap();
-		vs_path.push("shaders");
-		vs_path.push("geom.vs.glsl");
+//   print!("vertex: {:?}\n", vertex_shader);
+//   print!("fragment: {:?}\n", fragment_shader);
 
-		let mut fs_path = fs::canonicalize(".").unwrap();
-		fs_path.push("shaders");
-		fs_path.push("geom.vs.glsl");
-
-		print! ("vertex: {:?}\n", vs_path);
-		print! ("fragment: {:?}\n", fs_path);
-
-//		let mut f1 = try!(File::open(vs_path));
-//		let mut vertex_shader_buff = String::new();
-//		let len1 = f1.read_to_string(&mut vertex_shader_buff);
-//
-//		let mut f2 = try!(File::open(fs_path));
-//		let mut fragment_shader_buff = String::new();
-//		let len2 = f2.read_to_string(&mut fragment_shader_buff);
-//
-//		(vertex_shader_buff, fragment_shader_buff)
-//	};
+    let program = 
+        match (vertex_shader, fragment_shader) {
+            (Ok(a), Ok(b)) => glium::Program::from_source(&display, &a, &b, None).unwrap(),
+            _ => panic!("could not find shaders"),
+        };
 
 
-//	let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-//
-//    let mut t: f32 = -0.5;
-//
-//	loop {
-//
-//        t += 0.0002;
-//        if t > 0.5 {
-//            t = -0.5;
-//        }
-//
-//        let mut target = display.draw();
-//        target.clear_color(0.0, 0.0, 1.0, 1.0);
-//		// target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
-//		target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t }, &Default::default()).unwrap();
-//        target.finish().unwrap();
-//
-//		// listing the events produced by the window and waiting to be received
-//		for ev in display.poll_events() {
-//			match ev {
-//				glium::glutin::Event::Closed => return,   // the window has been closed by the user
-//				_ => ()
-//			}
-//		}
-//	}
+    let mut t: f32 = -0.5;
+
+	loop {
+
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+		// target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+		target.draw(&vertex_buffer, &indices, &program, &uniform! { t: t }, &Default::default()).unwrap();
+        target.finish().unwrap();
+
+		// listing the events produced by the window and waiting to be received
+		for ev in display.poll_events() {
+			match ev {
+				glium::glutin::Event::Closed => return,   // the window has been closed by the user
+				_ => ()
+			}
+		}
+	}
 
 }
